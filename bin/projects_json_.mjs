ba4b2648117d4be6972a540be0addc3299d477a7
Brian Takita <brian.takita@gmail.com>
2021-07-15 22:10:28 -0400

    ## 9.0.5
    ### Patch Changes
    
    - back to esm module with cjs using esm npm package to load library
    - Updated dependencies [undefined]
    - @ctx-core/function@20.0.5

--- bin/projects_json_.mjs
+++ bin/projects_json_.mjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { run } from '@ctx-core/run'
-import projects_json_ from '../dist/index.mjs'
+import projects_json_ from '../dist/index.js'
 await run(async () => {
 	console.info(JSON.stringify(await projects_json_(), null, 2))
 })

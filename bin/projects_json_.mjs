7e69772db4f0868e7c9302ca47583542dfd0be08
Brian Takita <brian.takita@gmail.com>
2021-12-28 17:12:59 -0600

    lib instead of dist

--- bin/projects_json_.mjs
+++ bin/projects_json_.mjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { run } from '@ctx-core/function'
-import projects_json_ from '../dist/index.js'
+import projects_json_ from '../lib/index.js'
 await run(async () => {
 	console.info(JSON.stringify(await projects_json_(), null, 2))
 })

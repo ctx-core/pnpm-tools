86c3046ce36e73c2eb48ceae21146f21c5f81720
Brian Takita <brian.takita@gmail.com>
2026-03-06 07:18:12 -0500

    chore: migrate scripts from pnpm to bun

--- projects_json_/index.js
+++ projects_json_/index.js
@@ -1,13 +1,12 @@
 import { flatten } from 'ctx-core/array'
 import fs from 'node:fs'
-import yaml from 'js-yaml'
 import { join } from 'node:path'
 import glob from 'tiny-glob'
 import { project_json_ } from '../project_json_/index.js'
 export async function project_json_a_() {
-	const workspace_yaml_buffer = await fs.promises.readFile('./pnpm-workspace.yaml')
-	const doc = yaml.load(workspace_yaml_buffer.toString())
-	const package_json_glob_a = doc.packages.map((doc_package)=>join('.', doc_package, 'package.json'))
+	const pkg_json = JSON.parse(await fs.promises.readFile('./package.json', 'utf-8'))
+	const workspaces = pkg_json.workspaces || []
+	const package_json_glob_a = workspaces.map((ws_pattern)=>join('.', ws_pattern, 'package.json'))
 	return Promise.all(package_json_glob_a.map(($)=>glob($))).then(($aa)=>flatten($aa)).then(($a)=>$a.map(($)=>project_json_($))).then(($a)=>Promise.all($a))
 }
 export { project_json_a_ as projects_json_ }
